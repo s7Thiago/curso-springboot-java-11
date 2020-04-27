aebed0bc13a241d72863b60c3b3fc49b5e2ccc9d
s7thiago <thyagosousasilva@gmail.com>
2020-04-27 14:29:31 -0300

    Service layer and component registation

---
+++ src/main/java/com/silva/thiago/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.silva.thiago.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.silva.thiago.course.entities.User;
+import com.silva.thiago.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user= repository.findById(id);
+		return user.get();
+	}
+
+}
