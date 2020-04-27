3390c0698c10081933e515d43ab9deca726467db
s7thiago <thyagosousasilva@gmail.com>
2020-04-26 22:03:21 -0300

    User entity and resource

---
+++ src/main/java/com/silva/thiago/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.silva.thiago.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.silva.thiago.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@email.com", "61012345678", "maria123");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
