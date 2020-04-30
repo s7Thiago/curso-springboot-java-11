be797075bcdaf3c0c2119d00f55186d5f074454c
s7thiago <thyagosousasilva@gmail.com>
2020-04-29 23:30:45 -0300

    Category

--- src/main/java/com/silva/thiago/course/config/TestConfig.java
+++ src/main/java/com/silva/thiago/course/config/TestConfig.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.silva.thiago.course.entities.Category;
 import com.silva.thiago.course.entities.Order;
 import com.silva.thiago.course.entities.User;
 import com.silva.thiago.course.entities.enums.OrderStatus;
+import com.silva.thiago.course.repositories.CategoryRepository;
 import com.silva.thiago.course.repositories.OrderRepository;
 import com.silva.thiago.course.repositories.UserRepository;
 
@@ -21,9 +23,18 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Eelctronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
