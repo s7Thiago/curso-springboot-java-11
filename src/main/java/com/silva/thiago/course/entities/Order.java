182ff2cf53675f5ad06c707d50d32d45691f63f5
s7thiago <thyagosousasilva@gmail.com>
2020-05-02 16:11:31 -0300

    Product-OrderITem one-to-many association

--- src/main/java/com/silva/thiago/course/entities/Order.java
+++ src/main/java/com/silva/thiago/course/entities/Order.java
@@ -16,7 +16,6 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.silva.thiago.course.entities.enums.OrderStatus;
 
 @Entity
